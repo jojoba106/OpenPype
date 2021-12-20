19e0366d9587110255b9338144df2c063e484222
iLLiCiTiT <jakub.trllo@gmail.com>
2021-12-20 19:29:29 +0100

    added new publisher implementation

---
+++ openpype/hosts/new_standalonepublisher/api/__init__.py
@@ -0,0 +1,18 @@
+from .pipeline import (
+    install,
+    ls,
+
+    save_project_name,
+    get_context_data,
+    update_context_data
+)
+
+
+__all__ = (
+    "install",
+    "ls",
+
+    "save_project_name",
+    "get_context_data",
+    "update_context_data"
+)
