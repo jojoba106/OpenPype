b2856864a8a804c2131335ae06eebfbab055855b
iLLiCiTiT <jakub.trllo@gmail.com>
2022-01-03 19:35:24 +0100

    added paint_image_with_color to tools/utils

--- openpype/tools/utils/__init__.py
+++ openpype/tools/utils/__init__.py
@@ -1,8 +1,13 @@
+from .lib import (
+    paint_image_with_color,
+)
 from .widgets import (
     PlaceholderLineEdit,
 )
 
 
 __all__ = (
+    "paint_image_with_color",
+
     "PlaceholderLineEdit",
 )
